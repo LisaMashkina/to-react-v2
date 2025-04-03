fac59350551fec30e20c1cba9a78aaa3b2dd5685
LisaMashkina <lisakaya919@gmail.com>
2025-04-03 21:02:55 +0300

    cards

--- src/components/cards/Master_card.jsx
+++ src/components/cards/Master_card.jsx
@@ -8,15 +8,16 @@ export default function Cake_card({ subs, name, URL, more }) {
     };
 
     return (
-        <div className="card">
+        <div className="card master">
             <div className="photo_block">
                 <img src={URL} className="photo"></img>
+                <div className="name">{String(name).toUpperCase()}</div>
             </div>
-            <div className="name">{name}</div>
+            
             <div className="text_block">
                 <div className="btns_row">
-                    <p className="text">{ `${subs} subs` }</p>
-                    <button className="btn_more buy">{`перейти >`}</button> 
+                    <p className="subs">{ `${subs} subs` }</p>
+                    <button className="btn_more go">{`перейти >`}</button> 
                 </div>
             
             </div>
